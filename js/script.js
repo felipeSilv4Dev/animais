a44fa49b718f881f9b67327295f116619b989b0a
felipeSilv4Dev <felipesilva.developer@gmail.com>
2023-03-09 13:27:04 -0300

    anima-numeros refatorados

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initAnimacaoScroll from "./modules/animacao-scroll.js";
 
 const scrollSuave = new ScrollSuave("[data-menu='suave'] a[href^='#']");
@@ -36,5 +35,4 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
-initAnimaNumeros();
 initAnimacaoScroll();
